17ee05c8d054c7baf5d098f1eae7134b473e8ceb
prashanth-pro <prashanth.gunasekaran@outlook.com>
2022-10-06 13:31:34 +0530

    initial commit

---
+++ src/main/java/com/prashanth/pro/springazuredemo/SpringAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package com.prashanth.pro.springazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hurray! You deployed a spring boot application in Azure. Whats next?";
+	}
+
+}
